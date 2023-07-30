c526728ade6b157a837d624a5caed2fe83a59f33
Harsh Rajput <iamharshrajput@gmail.com>
2023-07-30 12:02:09 +0530

    Done some minor fixes and Updated About Page

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,12 +1,14 @@
-const stripe = require('stripe')('sk_test_51NZAXDSIaxv6mfvnOawieSjoeuBmHnxKKfhnghqXexMZRWC1puW1ZzyKQucPTjZiMUt5JFdz8vr8n1orjiPpLW5900JPfjHuGn');
+require("dotenv").config();
 
-const Order = require('../models/order.model');
-const User = require('../models/user.model');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const Order = require("../models/order.model");
+const User = require("../models/user.model");
 
 async function getOrders(req, res) {
   try {
     const orders = await Order.findAllForUser(res.locals.uid);
-    res.render('customer/orders/all-orders', {
+    res.render("customer/orders/all-orders", {
       orders: orders,
     });
   } catch (error) {
@@ -36,38 +38,38 @@ async function addOrder(req, res, next) {
   req.session.cart = null;
 
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
-    line_items: cart.items.map(function(item) {
-      return  {
+    payment_method_types: ["card"],
+    line_items: cart.items.map(function (item) {
+      return {
         price_data: {
-          currency: 'INR',
+          currency: "INR",
           product_data: {
-            name: item.product.title
+            name: item.product.title,
           },
-          unit_amount: +item.product.price.toFixed(2) * 100
+          unit_amount: +item.product.price.toFixed(2) * 100,
         },
         quantity: item.quantity,
-      }
+      };
     }),
-    mode: 'payment',
-    success_url: `http://localhost:3000/orders/success`,
-    cancel_url: `http://localhost:3000/orders/failure`,
+    mode: "payment",
+    success_url: `${process.env.DOMAIN_URL}/orders/success`,
+    cancel_url: `${process.env.DOMAIN_URL}/orders/failure`,
   });
 
   res.redirect(303, session.url);
 }
 
 function getSuccess(req, res) {
-  res.render('customer/orders/success');
+  res.render("customer/orders/success");
 }
 
 function getFailure(req, res) {
-  res.render('customer/orders/failure');
+  res.render("customer/orders/failure");
 }
 
 module.exports = {
   addOrder: addOrder,
   getOrders: getOrders,
   getSuccess: getSuccess,
-  getFailure: getFailure
+  getFailure: getFailure,
 };
